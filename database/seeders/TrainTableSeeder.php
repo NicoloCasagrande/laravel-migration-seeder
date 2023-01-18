91614b5bb1bb9e7705a4a736cad4621c36d39032
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-01-18 17:00:49 +0100

    creazione campi con seeder

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainTableSeeder extends Seeder
+{
+    public function run(Faker $faker)
+    {
+        
+        for ($i=0; $i <= 30; $i++) { 
+            //istazio un oggetto del modello
+            $new_train = new Train();
+    
+            $new_train->company = $faker->word();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->dateTime();
+            $new_train->arrival_time = $faker->dateTime();
+            $new_train->code = $faker->numberBetween(0,100) . $faker->randomLetter() . $faker->randomLetter();
+            $new_train->wagon = $faker->randomDigit();
+            $new_train->is_in_time = rand(0,1);
+            $new_train->is_cancelled = rand(0,1);
+            //salva il recordo nel DB
+            $new_train->save();
+        }
+    }
+}
